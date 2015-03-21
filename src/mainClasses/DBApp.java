867a0efb6c4813691f36beec98e3a646654c7b09
Nader <ahmednader839@gmail.com>
2015-03-22 00:48:28 +0100

    insert values into .ser file but still incomplete

--- src/mainClasses/DBApp.java
+++ src/mainClasses/DBApp.java
@@ -1,14 +1,23 @@
 package mainClasses;
 
 import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
+
+import com.sun.corba.se.impl.io.OptionalDataException;
 
 import Exceptions.DBAppException;
 import Exceptions.DBEngineException;
@@ -17,6 +26,8 @@ public class DBApp {
 	
 	static boolean validTableName = true;
 	static boolean metadataIsCreated = false;
+	static String metadata = "metadata";
+	static String serFile = "records.ser";
 	
 	public void init()
 	{
@@ -31,7 +42,7 @@ public class DBApp {
 		
 		try
 		{
-			fileReader = new BufferedReader(new FileReader("metadata"));
+			fileReader = new BufferedReader(new FileReader(metadata));
 		}
 		catch(Exception e)
 		{
@@ -45,7 +56,7 @@ public class DBApp {
 			String line = "";
 			
 			
-			fileReader.readLine();
+		
 			
 			while ((line = fileReader.readLine()) != null)
 			{
@@ -118,7 +129,7 @@ public class DBApp {
 	    	try
 		    {
 		    	
-		    	fileWriter = new FileWriter("metadata", true);
+		    	fileWriter = new FileWriter(metadata, true);
 		    	metadataIsCreated = true;
 		    	//System.out.print(metadataIsCreated);
 		    	
@@ -215,10 +226,158 @@ public class DBApp {
 		
 	}
 	
-	public void insertIntoTable(String strTableName, Hashtable<String, String> htblColNameValue) throws DBAppException
+	public static boolean tableExists(String strTableName) {
+		BufferedReader fileReader = null;
+		String COMMA_DELIMITER = ",";
+		
+		try
+		{
+			fileReader = new BufferedReader(new FileReader(metadata));
+		}
+		catch(Exception e)
+		{
+			
+			return false;
+			
+		}
+		
+		try
+		{
+			String line = "";
+			while ((line = fileReader.readLine()) != null)
+			{
+				
+				String[] tokens = line.split(COMMA_DELIMITER);
+				if (tokens.length > 0)
+				{
+					if(tokens[0].equals(strTableName))
+					{
+						return true;
+					}
+				}
+			}
+		} 
+		catch(Exception e)
+		{
+			System.out.println("Error in CsvFileReader !!!");
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public static boolean colExists(String strTableName , String Colname) {
+		BufferedReader fileReader = null;
+		String COMMA_DELIMITER = ",";
+		
+		try
+		{
+			fileReader = new BufferedReader(new FileReader(metadata));
+		}
+		catch(Exception e)
+		{
+			
+			return false;
+			
+		}
+		
+		try
+		{
+			String line = "";
+			while ((line = fileReader.readLine()) != null)
+			{
+				
+				String[] tokens = line.split(COMMA_DELIMITER);
+				if (tokens.length > 0)
+				{
+					if(tokens[0].equals(strTableName))
+					{
+						if (tokens[1].equals(Colname))
+						return true;
+					}
+				}
+			}
+		} 
+		catch(Exception e)
+		{
+			System.out.println("Error in CsvFileReader !!!");
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public static void insertIntoTable(String strTableName, Hashtable<String, String> htblColNameValue) 
+	throws DBAppException, IOException
+	// still need to check if the value inserted is of the same type as the column, 
+	// and set the max number of records in a file
+	// search for a way to keep track of each record inserted to be able to retrieve it 
 	{
+		if (!tableExists(strTableName)) 
+		{
+			System.out.println("Table doesn't exist");
+		}
+		else 
+		{
+	    Enumeration e1 = htblColNameValue.keys();
+		List<String> Colname = new ArrayList();
+		List<String> Value = new ArrayList();
+		while (e1.hasMoreElements()) 
+			{
+				String key = (String) e1.nextElement();
+				  if (!colExists(strTableName, key))
+				  {
+					  System.out.println("Invalid column name" + key);
+					  return;  
+				  }
+			      Colname.add(key);
+			      Value.add(htblColNameValue.get(key));
+		    }
+		try 
+		{
+		FileOutputStream fileOut = new FileOutputStream(serFile,true);
+		ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		 for (int i = 0; i < Colname.size(); i++)
+    	{
+			out.writeObject(Colname.get(i));
+			out.writeObject(Value.get(i));
+    	}
+		 out.close();
+		 fileOut.close();
+		}
+		catch(IOException i)
+	      {
+	          i.printStackTrace();
+	      }
 		
 	}
+	}
+	public static void retrieve() throws IOException, ClassNotFoundException{
+		
+	         
+	         String Colname = ""; 
+	         ObjectInputStream in = null;
+	         try {
+	        	 FileInputStream fileIn = new FileInputStream(serFile);
+		         in = new ObjectInputStream(fileIn);
+	         while(true) 
+	     	{
+	        	 
+	        		 Colname = (String)in.readObject();
+	 	 			System.out.println(Colname);
+	        	 
+	     	}
+	         }
+	         catch (Exception e) {
+	        	 
+	        		 System.out.println("File is Empty");
+	         }
+	         finally {
+	         
+	        	 if (in != null)
+	                 in.close();
+	         }
+	      }
+		
+	
 	
 	public void deleteFromTable(String strTableName, Hashtable<String, String> htblColNameValue, 
 			String strOperator) throws DBEngineException
@@ -245,7 +404,7 @@ public class DBApp {
 	
 	
 	//Just to test what has been implemented so far.
-	public static void main(String[] args) throws DBAppException
+	public static void main(String[] args) throws DBAppException, IOException , ClassNotFoundException
 	{
 		Hashtable h1 = new Hashtable();
 		h1.put("ID", "java.lang.Integer");
@@ -256,8 +415,13 @@ public class DBApp {
 		
 		Hashtable h2 = new Hashtable();
 		h2.put("Dept", "Department.ID");
+		
+		
 		h2.put("Country", "Country.ID");
 		//createTable("Employee", h1, h2, "ID");
+		//System.out.println(tableExists("Employe"));
+		//System.out.println(colExists("Employee","I"));
+		
 		
 		Hashtable h3 = new Hashtable();
 		h3.put("id", "java.lang.Integer");
@@ -266,6 +430,18 @@ public class DBApp {
 		h3.put("Datum", "java.util.Date");
 		//h3.put("Stadt", "java.util.String");
 		//createTable("Kellner", h3, h2, "id");
+		
+		Hashtable h4 = new Hashtable();
+		h4.put("ID", "1");
+		h4.put("Name", "ahmed");
+		h4.put("Dept", "cs");
+		h4.put("Start_Date", "january");
+		h4.put("Country", "egypt");
+		
+		
+		insertIntoTable("Employee", h4);
+		retrieve();
+		
 	}
 	
 }
